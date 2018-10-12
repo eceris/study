a6fcc7f413153d96cc14a3edff619c8e6394a700
eceris <eceris@gmail.com>
2018-10-12 16:16:50 +0900

    [R2DBC] 샘플링 코드 커밋

---
+++ reactive/coffee-service/src/main/java/kr/co/eceris/r2dbc/coffeeservice/CoffeeServiceApplication.java
@@ -0,0 +1,103 @@
+package kr.co.eceris.r2dbc.coffeeservice;
+
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.r2dbc.function.DatabaseClient;
+import org.springframework.data.r2dbc.repository.support.R2dbcRepositoryFactory;
+import org.springframework.data.relational.core.mapping.RelationalMappingContext;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+
+
+@SpringBootApplication
+public class CoffeeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeServiceApplication.class, args);
+    }
+
+    @RequiredArgsConstructor
+    @Component
+    class DataLoader {
+        private final CoffeeRepository repo;
+
+        @PostConstruct
+        private void load() {
+//            repo.deleteAllById().thenMany(
+//                    Flux.just("Peet's Coffee", "Philz Coffee", "Blue Bottle Coffee")
+//                            .map(Coffee::new)
+//                            .flatMap(repo::save))
+//                    .thenMany(repo.findAll())
+//                    .subscribe(System.out::println);
+
+            Flux.just("Peet's Coffee", "Philz Coffee", "Blue Bottle Coffee")
+                    .map(Coffee::new)
+                    .flatMap(repo::save)
+                    .doOnError(System.out::println)
+                    .subscribe(System.out::println);
+        }
+    }
+
+    @Configuration
+    class DatabaseConfig {
+        @Bean
+        PostgresqlConnectionFactory connectionFactory() {
+            return new PostgresqlConnectionFactory(
+                    PostgresqlConnectionConfiguration.builder()
+                            .host("localhost")
+                            .database("groupoffice")
+                            .username("groupoffice")
+                            .password("groupoffice")
+                            .build());
+        }
+
+        @Bean
+        DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
+            return DatabaseClient.builder()
+                    .connectionFactory(connectionFactory)
+                    .build();
+        }
+
+        @Bean
+        R2dbcRepositoryFactory repositoryFactory(DatabaseClient client) {
+            RelationalMappingContext context = new RelationalMappingContext();
+            context.afterPropertiesSet();
+
+            return new R2dbcRepositoryFactory(client, context);
+        }
+
+        @Bean
+        CoffeeRepository coffeeRepository(R2dbcRepositoryFactory factory) {
+            return factory.getRepository(CoffeeRepository.class);
+        }
+    }
+
+    interface CoffeeRepository extends ReactiveCrudRepository<Coffee, Long> {
+        @Query("DELETE FROM coffee")
+        Mono<Coffee> deleteAllById();
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    class Coffee {
+        @Id
+        private Long id;
+        @NonNull
+        private String name;
+    }
+}
+
---
+++ reactive/coffee-service/src/test/java/kr/co/eceris/r2dbc/coffeeservice/CoffeeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.eceris.r2dbc.coffeeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoffeeServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
