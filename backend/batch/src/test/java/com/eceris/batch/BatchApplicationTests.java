6547ba3bb02dbb68b1d5c04de56b917cae57aa40
eceris <eceris@gmail.com>
2020-04-01 16:40:28 +0900

    [batch] h2 db tcp연결로 붙어서 볼수 있도록 처리

--- backend/batch/src/test/java/com/eceris/batch/BatchApplicationTests.java
+++ backend/batch/src/test/java/com/eceris/batch/BatchApplicationTests.java
@@ -3,11 +3,21 @@ package com.eceris.batch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootTest
 class BatchApplicationTests {
 
     @Test
     void contextLoads() {
+
+        LocalDate baseDateTime = LocalDate.parse("20200301", DateTimeFormatter.ofPattern("yyyyMMdd")); //어제
+        System.out.println(baseDateTime);
+        System.out.println(baseDateTime.toString());
+        System.out.println(baseDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+//        LocalDateTime todayDateTime = LocalDateTime.parse("2020-03-01", DateTimeFormatter.ofPattern("yyyyMMdd")).plusDays(1); //오늘
     }
 
 }
