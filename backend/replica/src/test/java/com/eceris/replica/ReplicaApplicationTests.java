6023590a5c3a0bce5427cb789af32b37080c3a6d
eceris <eceris@gmail.com>
2020-04-02 16:00:33 +0900

    [replication] add build.gradle

---
+++ backend/replica/src/test/java/com/eceris/replica/ReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eceris.replica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
